1779b9dbef43721155db8034e5a944252e534f69
Sébastien Saunier <seb@saunier.me>
2018-07-12 15:33:15 +0200

    Remove models

--- models.py
+++ models.py
@@ -4,7 +4,6 @@ class Product(db.Model):
     __tablename__ = "products"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String())
-    # description = db.Column(db.Text())
 
     def __repr__(self):
         return f"<id {self.id} - {self.name}>"
